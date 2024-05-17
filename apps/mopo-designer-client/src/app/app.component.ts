f48ef823d24dce220e264f15001ede9eca33a257
Sebastian Gsellmann <sirvannos@hotmail.com>
2024-05-17 14:52:09 -0400

    feat: layout updates

--- apps/mopo-designer-client/src/app/app.component.ts
+++ apps/mopo-designer-client/src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, MatSidenavModule],
+  imports: [RouterModule, MatToolbarModule, MatSidenavModule, MatDividerModule, MatListModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
