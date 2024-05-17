627b7b3554e6048f33270e4c9e75474be3c9922d
Sebastian Gsellmann <sirvannos@hotmail.com>
2024-05-17 15:39:18 -0400

    feat: engine loading

--- apps/mopo-designer-client/src/app/app.component.ts
+++ apps/mopo-designer-client/src/app/app.component.ts
@@ -4,15 +4,23 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
 
+import { RuleEngine } from '@mopopinball/engine/src/system/rule-engine/rule-engine';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, MatSidenavModule, MatDividerModule, MatListModule],
+  imports: [RouterModule, CommonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatDividerModule, MatListModule, MatSelectModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mopo-designer-client';
+  engines: RuleEngine[] = [
+    new RuleEngine('root', true, null),
+    new RuleEngine('attract', true, null)
+  ];
+  engine: RuleEngine = null;  
 }
