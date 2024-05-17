6d96420cd3d9b3d94eef8091fe8981735ce08c03
Sebastian Gsellmann <sirvannos@hotmail.com>
2024-05-17 14:07:08 -0400

    feat: add angular material

--- apps/mopo-designer-client/src/app/app.component.ts
+++ apps/mopo-designer-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
